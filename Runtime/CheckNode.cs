566487d35d8b9ca220a4fb54e620495ac21addf8
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-10-18 00:15:03 -0500

    Added: CheckNode

---
+++ Runtime/CheckNode.cs
@@ -0,0 +1,32 @@
+﻿namespace FinTOKMAK.TimelineSystem.Runtime
+{
+    /// <summary>
+    /// The check node type
+    /// CheckAnim: check the present of anim clip
+    /// CheckEvent: check the present of certain event
+    /// </summary>
+    public enum CheckNodeType
+    {
+        CheckAnim,
+        CheckEvent
+    }
+    
+    [System.Serializable]
+    public class CheckNode
+    {
+        #region Public Field
+
+        /// <summary>
+        /// The node type of the CheckNode
+        /// </summary>
+        public CheckNodeType nodeType;
+
+        /// <summary>
+        /// The animation clip to play
+        /// Or the event name to check
+        /// </summary>
+        public string field;
+
+        #endregion
+    }
+}
