3a3bf654ade898dfcb1cf3c618569230feccad8a
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-10-17 23:03:22 -0500

    Added: Core Timeline System

---
+++ Runtime/TimelineSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace FinTOKMAK.TimelineSystem.Runtime
+{
+    public class TimelineSystem : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
